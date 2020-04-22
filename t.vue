c5e1308e3b4cfc1aae62015cc3ff4f6e67bfc06b
cordis-dev <darius@adroiti.com>
2020-04-22 15:43:35 +0300

    Create t.vue

---
+++ t.vue
@@ -0,0 +1,8 @@
+<template>
+  <!-- ✓ GOOD -->
+  <div>{{ text }}</div>
+  <div>{{   text   }}</div><!-- Use vue/no-multi-spaces rule to disallow multiple spaces. -->
+
+  <!-- ✗ BAD -->
+  <div>{{text}}</div>
+</template>
